e8834ccbfa00f6fa46b6fbace02c4d4f9c9f0dcd
nhat0120230 <nhatnguyen221020032@gmail.com>
2024-01-18 11:04:57 +0700

    thuc hanh

---
+++ thuchanh.sql
@@ -0,0 +1,35 @@
+create database QuanLiDiemThi;
+
+use QuanLiDiemThi;
+
+create table HocSinh(
+MaHS varchar(20) primary key,
+TenHS varchar(50),
+NgaySinh datetime,
+Lop varchar(20),
+GT varchar(20)
+);
+
+create table GiaoVien(
+MaGV varchar(20) primary key,
+TenGV varchar(50),
+SDT varchar(10)
+);
+
+create table MonHoc(
+MaMH varchar(20) primary key,
+TenMH varchar(50),
+MaGV varchar(20)
+);
+
+create table BangDiem(
+MaHS varchar(20),
+MaMH varchar(20),
+DiemThi int,
+NgayKT datetime,
+primary key (MaHS, MaMH),
+foreign key (MaHS) references HocSinh(MaHS),
+foreign key (MaMH) references MonHoc(MaMH)
+);
+
+alter table monhoc add constraint FK_MaGV foreign key (MaGV) references giaovien(MaGV);
